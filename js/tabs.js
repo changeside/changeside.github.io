49feb32ec452dcd19e38bd75ef4b7ef9cc9b81a2
greyliances <36825953+greyliances@users.noreply.github.com>
2024-03-11 20:15:39 +0100

    Better Tab Handling
    Fixes an error when no URLSearchParam was set for "tab"
    Moves JS code for tab handling into its own file for better maintainability
    
    Changes the tab SearchParam when opening specific tabs
    
    Tab sections are now given a custom data attribute named "project-id" which matches a project in the sidebar list
    
    i.e.
     <div project-id="anjellyca" class="tab"> for the tab element
    <li class="tablinks" id="anjellyca">Anjellyca</li> for the sidebar entry

---
+++ js/tabs.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", async (event) => {
+  const sidebarProjectsList = document
+    .getElementById("linktabs")
+    .querySelectorAll(".tablinks");
+
+  const projectsList = document
+    .getElementById("content")
+    .querySelectorAll(".tab");
+
+  let tab = getURLSearchParam("tab") || "home";
+  switchTab(tab);
+
+  sidebarProjectsList.forEach((project) => {
+    project.onclick = () => switchTab(project.id);
+  });
+
+  function switchTab(tabName) {
+    sidebarProjectsList.forEach((project) => {
+      project.id !== tabName
+        ? project.classList.remove("active")
+        : project.classList.add("active");
+    });
+
+    projectsList.forEach((projectTab) => {
+      projectTab.style.display =
+        projectTab.getAttribute("project-id") !== tabName ? "none" : "block";
+    });
+
+    if (
+      "URLSearchParams" in window &&
+      "history" in window &&
+      "replaceState" in window.history
+    ) {
+      try {
+        let searchParams = new URLSearchParams(window.location.search);
+        searchParams.set("tab", tabName);
+        window.history.replaceState(null, "", "?" + searchParams.toString());
+      } catch (error) {
+        /**
+         * Might be missing secure-context if not served via https.
+         * In this case we just don't modify the search params
+         */
+      }
+    }
+
+    document
+      .getElementById("content")
+      .querySelectorAll("video")
+      .forEach((videoElem) => {
+        try {
+          if (typeof videoElem.pause === "function") {
+            videoElem.pause();
+          }
+        } catch (error) {
+          console.warn(`Couldn't pause the following VideoElement`);
+          console.warn(videoElem);
+        }
+      });
+  }
+});
+
+function getURLSearchParam(paramName) {
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split("=");
+    if (pair[0] === paramName) {
+      return pair[1];
+    }
+  }
+  return false;
+}
